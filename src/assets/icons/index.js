bacc8cc7093df60e1cf0e538482b9513044d59bb
Arden <ardenerc@gmail.com>
2021-03-21 21:14:09 +0300

    pirlanta ikonları eklendi

---
+++ src/assets/icons/index.js
@@ -0,0 +1,14 @@
+import Asscher from './asscher.svg';
+import Cushion from './cushion.svg';
+import Emerald from './emerald.svg';
+import Heart from './heart.svg';
+import Markiz from './markiz.svg';
+import Oval from './oval.svg';
+import Pear from './pear.svg';
+import Princess from './princess.svg';
+import Radiant from './radiant.svg';
+import Round from './round.svg';
+import SquareRadiant from './square-radiant.svg';
+import NoData from './no-data.svg';
+
+export {Asscher, Cushion, Emerald, Heart, Markiz, Oval, Pear, Princess, Radiant, Round, SquareRadiant, NoData};
