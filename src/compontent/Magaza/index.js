42f374b9efa3d164cc910848e8cef79bc8cfa584
Arden <ardenerc@gmail.com>
2021-03-21 21:15:24 +0300

    pirlanta ve magaza componentlar index js ile export ediliyor

---
+++ src/compontent/Magaza/index.js
@@ -0,0 +1,6 @@
+import MagazaListItem from '../MagazaListItem';
+import Envanter from './Envanter';
+import Header from './Header';
+import MagazaOverflowMenu from './MagazaOverflowMenu';
+
+export {MagazaListItem, Envanter, Header, MagazaOverflowMenu};
---
+++ src/compontent/pirlanta/index.js
@@ -0,0 +1,11 @@
+import PirlantaListItem, {PirlantaCard} from './PirlantaListItem';
+import PirlantaOverflow from './PirlantaOverflowMenu';
+import {SearchButtonGroup} from './SearchButtonGroup';
+import SelectModal from './SelectModal';
+export {
+  PirlantaOverflow,
+  PirlantaListItem,
+  SelectModal,
+  PirlantaCard,
+  SearchButtonGroup,
+};
