b540ebbbd18e36d696a48d8bf1f771f9fd57c666
Arden <ardenerc@gmail.com>
2021-03-21 21:16:55 +0300

    searchbuttongroup selectcomponent altında alındı

--- src/compontent/pirlanta/SearchButtonGroup.js
+++ src/compontent/pirlanta/SearchButtonGroup.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import {StyleSheet, Text} from 'react-native';
 import {Button, ButtonGroup, Icon, Layout} from '@ui-kitten/components';
-
+import {queryClient} from '../../helper/query-client';
 const SearchOutlineIcon = (props) => <Icon {...props} name="search-outline" />;
 const FilterIcon = (props) => <Icon {...props} name="funnel-outline" />;
 const SaveIcon = (props) => <Icon {...props} name="save-outline" />;
 const DeleteIcon = (props) => <Icon {...props} name="trash-2-outline" />;
 
-export const SearchButtonGroup = () => (
+const clearQueryData = (queryClient, key) => {
+  queryClient.setQueryData(key, null);
+};
+// sort karat adet fiyat
+
+export const SearchButtonGroup = ({onPress, onSortPress}) => (
   <Layout style={styles.container}>
-    <Button
-      size="small"
-      style={styles.button}
-      accessoryLeft={SearchOutlineIcon}>
+    <Button size="small" onPress={onPress} style={styles.button} accessoryLeft={SearchOutlineIcon}>
       Ara
     </Button>
-    <Button size="small" style={styles.button} accessoryLeft={FilterIcon}>
+    <Button size="small" onPress={onSortPress} style={styles.button} accessoryLeft={FilterIcon}>
       Sırala
     </Button>
     <Button size="small" style={styles.button} accessoryLeft={SaveIcon}>
       Kaydet
     </Button>
-    <Button size="small" style={styles.button} accessoryLeft={DeleteIcon}>
+    <Button size="small" onPress={() => clearQueryData(queryClient, 'pirlantalar')} style={styles.button} accessoryLeft={DeleteIcon}>
       Temizle
     </Button>
   </Layout>
