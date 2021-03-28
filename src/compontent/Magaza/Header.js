f7e5a79047c66a2206d70339a40f192203db1360
Arden <ardenerc@gmail.com>
2021-03-29 02:50:21 +0300

    duruşu düzenlendi

--- src/compontent/Magaza/Header.js
+++ src/compontent/Magaza/Header.js
@@ -1,31 +1,22 @@
 import React from 'react';
-import {
-  Text,
-  ListItem,
-  Icon,
-  Divider,
-  Input,
-  Layout,
-  Avatar,
-} from '@ui-kitten/components';
+import {Text, ListItem, Icon, Divider, Input, Layout, Avatar, Card} from '@ui-kitten/components';
 import {View, StyleSheet} from 'react-native';
+import {StyledText} from '../StyledText';
+import {Image} from 'react-native-svg';
 
 const PhoneIcon = (props) => <Icon {...props} name="smartphone-outline" />;
 const MailIcon = (props) => <Icon {...props} name="email-outline" />;
 const Header = ({data, children}) => (
   <Layout>
-    <Layout>
-      <Layout level="1" style={styles.headerContainer}>
-        <Avatar size="giant" source={require('../../../assets/tst.png')} />
-        <Text category="h4">{data.name}</Text>
-        {children}
-      </Layout>
-      <Divider />
-      <ListItem title={data.number} accessoryLeft={PhoneIcon}></ListItem>
-      <Divider />
-      <ListItem title={data.adres} accessoryLeft={MailIcon}></ListItem>
-      <Divider />
+    <Layout level="1" style={styles.headerContainer}>
+      <Text category="h4">{data.name}</Text>
+      {children}
     </Layout>
+    <Divider />
+    <ListItem title={() => <StyledText>{data.number}</StyledText>} accessoryLeft={PhoneIcon}></ListItem>
+    <Divider />
+    <ListItem title={() => <StyledText>{data.adres}</StyledText>} accessoryLeft={MailIcon}></ListItem>
+    <Divider />
   </Layout>
 );
 
@@ -33,9 +24,8 @@ const styles = StyleSheet.create({
   headerContainer: {
     margin: 20,
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    position: 'relative',
     marginBottom: 5,
+    justifyContent: 'space-between',
   },
 });
 
